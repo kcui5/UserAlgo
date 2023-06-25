df33dc87cc364f63ae0ed4f3faca0e345a1482a2
Kyle <kyle.tz.cui@gmail.com>
2023-06-25 16:17:49 -0700

    new stuff

--- background.js
+++ background.js
@@ -1,22 +1,27 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeNetRequest.updateRule({
-    addRules: [
-      {
-        id: 'block_requests_with_keywords',
-        priority: 1,
-        action: { type: 'block' },
-        condition: {
-          urlFilter: '*://www.youtube.com/*',
-          resourceTypes: ['main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'font', 'object', 'xmlhttprequest', 'fetch'],
-          // The list of keywords to block
-          // Modify this array to add or remove keywords to block
-          urlMatches: [
-            '*keyword1*',
-            '*keyword2*',
-            '*keyword3*'
-          ]
-        }
-      }
-    ]
-  });
+chrome.webNavigation.onCompleted.addListener((details) => {
+  console.log("Loaded listener");
+  console.log(details.url);
+  if (details.url.startsWith("https://www.youtube.com/results")) {
+    console.log("results");
+    chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      function: blockYouTubeSearchResults
+    });
+  }
 });
+
+function blockYouTubeSearchResults() {
+  console.log("Looking to block");
+  const blockedKeywords = chrome.storage.sync.get("blockedKeywords");
+  blockedKeywords = ["chicken"]
+
+  const searchResults = document.querySelectorAll("#contents ytd-video-renderer");
+  searchResults.forEach((result) => {
+    const titleElement = result.querySelector("#video-title");
+    const title = titleElement.innerText.toLowerCase();
+
+    if (blockedKeywords.some(keyword => title.includes(keyword))) {
+      result.remove();
+    }
+  });
+}
