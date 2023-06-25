4969e6f7cf26bba1bfac0df5b1542c83aa759c3a
Kyle <kyle.tz.cui@gmail.com>
2023-06-25 16:32:33 -0700

    dunno

--- background.js
+++ background.js
@@ -1,27 +1,21 @@
 chrome.webNavigation.onCompleted.addListener((details) => {
-  console.log("Loaded listener");
-  console.log(details.url);
-  if (details.url.startsWith("https://www.youtube.com/results")) {
-    console.log("results");
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      function: blockYouTubeSearchResults
-    });
-  }
-});
-
-function blockYouTubeSearchResults() {
-  console.log("Looking to block");
-  const blockedKeywords = chrome.storage.sync.get("blockedKeywords");
-  blockedKeywords = ["chicken"]
-
-  const searchResults = document.querySelectorAll("#contents ytd-video-renderer");
-  searchResults.forEach((result) => {
-    const titleElement = result.querySelector("#video-title");
-    const title = titleElement.innerText.toLowerCase();
-
-    if (blockedKeywords.some(keyword => title.includes(keyword))) {
-      result.remove();
+  chrome.tabs.get(details.tabId, (tab) => {
+    console.log("Loaded listener");
+    console.log(details.url);
+    if (tab.url.startsWith("https://www.youtube.com/results")) {
+      console.log("results");
+      var blockedKeywords = chrome.storage.sync.get("blockedKeywords");
+      blockedKeywords = ["chicken"]
+      console.log("chicken")
+      const searchResults = tab.querySelectorAll("#contents ytd-video-renderer");
+      searchResults.forEach((result) => {
+        const titleElement = result.querySelector("#video-title");
+        const title = titleElement.innerText.toLowerCase();
+        console.log(title)
+        if (blockedKeywords.some(keyword => title.includes(keyword))) {
+          result.remove();
+        }
+      });
     }
-  });
-}
+  })
+});
